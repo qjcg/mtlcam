49a79fa0a24bc302bb7bef1a4f8f1f6927f92a4c
John Gosset <john.gosset@gmail.com>
2016-01-25 02:26:31 -0500

    Added python and shell implementations

---
+++ mtlcam.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Download city camera open data
+
+URL_GEOJSON="http://ville.montreal.qc.ca/circulation/sites/ville.montreal.qc.ca.circulation/files/cameras-de-circulation.json"
+
+base_dir=${1:-mtlcam}
+full_dir="${base_dir}/$(date +'%y%m%d/%H%M%S')"
+cache="${base_dir}/cameras-de-circulation.json"
+max_concurrency=${2:-10}
+
+mkdir -p $full_dir
+if [[ ! -r ${cache} ]]; then
+	curl -s $URL_GEOJSON -o ${cache}
+fi
+
+cat "${cache}" |
+	jq '. .features[] | .properties."url-image-en-direct"' |
+	xargs -n ${max_concurrency} wget -P ${full_dir} -i -
