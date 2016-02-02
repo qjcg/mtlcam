60835d81196c4957848d3c8f8b15067fbebd4ba7
John Gosset <john.gosset@gmail.com>
2016-02-02 14:08:25 -0500

    Use aria2c for concurrent downloading in mtlcam.sh

--- mtlcam.sh
+++ mtlcam.sh
@@ -6,13 +6,14 @@ URL_GEOJSON="http://ville.montreal.qc.ca/circulation/sites/ville.montreal.qc.ca.
 base_dir=${1:-images}
 full_dir="${base_dir}/$(date +'%y%m%d/%H%M%S')"
 cache="${base_dir}/cameras-de-circulation.json"
-max_concurrency=${2:-10}
+max_concurrency=${2:-90}
 
 mkdir -p $full_dir
 if [[ ! -r ${cache} ]]; then
 	curl -s $URL_GEOJSON -o ${cache}
 fi
 
-cat "${cache}" |
-	jq '. .features[] | .properties."url-image-en-direct"' |
-	xargs -n ${max_concurrency} wget -P ${full_dir} -i -
+# aria2c needs "dns" in /etc/nsswitch.conf
+# Ref: https://bugzilla.redhat.com/show_bug.cgi?id=1140135
+jq -r '. .features[] | .properties."url-image-en-direct"' "${cache}" |
+	aria2c -j ${max_concurrency} -d ${full_dir} -i -
